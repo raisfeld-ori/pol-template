3ae1765c25b91197ae6b51422523a33f9e61d69c
Fravy <arirl.elkaslasi@gmail.com>
2024-05-02 11:47:17 +0300

    RAHH!!!!!

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,46 @@ export default function Home() {
       <div 
       style={{transition: transition + "ms", opacity: opacity.toString()}}
       onClick={next_page}>
-        <h1 className="header">FullName's</h1>
+
+
+        <h1 className="header">Yourname's</h1>
         <h1 className="header2">Portfilio</h1>
+
+        <div className="sp-container">
+      <div className="sp-content">
+        <div className="sp-globe"></div>
+        <h2 className="frame-1">Talent #1</h2>
+        <h2 className="frame-2">Talent #2</h2>
+        <h2 className="frame-3">Talent #3</h2>
+        <h2 className="frame-4">Talent #4</h2>
+        <h2 className="frame-5">
+
+          
+          <span>Erm,</span>
+          <span> What the,</span>
+          <span> Sigma.</span>
+        </h2>
+      </div>
+    </div>
+    <div className="animation-container">
+      <div className="lightning-container">
+        <div className="lightning white"></div>
+        <div className="lightning red"></div>
+      </div>
+      <div className="boom-container">
+        <div className="shape circle big white"></div>
+        <div className="shape circle white"></div>
+        <div className="shape triangle big yellow"></div>
+        <div className="shape disc white"></div>
+        <div className="shape triangle blue"></div>
+      </div>
+      <div className="boom-container second">
+        <div className="shape circle big white"></div>
+        <div className="shape circle white"></div>
+        <div className="shape disc white"></div>
+        <div className="shape triangle blue"></div>
+      </div>
+    </div>
         <p className="clicktomain">Click Anywhere to begin!</p>
       </div>
     </main>
