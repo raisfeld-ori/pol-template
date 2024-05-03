30389f41a2afbc006c7118d5dabae067b2db1568
ori raisfeld <raisfeldori@gmail.com>
2024-05-03 18:56:13 +0300

    added spped insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body>
         {children}
         <footer className="footer1">Made by <span className="ftspn">Ariel Elkaslasi</span> and <span className="ftspn">Ori Risfeld</span>. </footer>
