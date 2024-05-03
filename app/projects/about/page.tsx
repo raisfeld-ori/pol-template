82f84a6b8d945b2d0e2c4f77312aafb2cfddb38a
ori raisfeld <raisfeldori@gmail.com>
2024-05-03 17:00:46 +0300

    added html to the about page

--- app/projects/about/page.tsx
+++ app/projects/about/page.tsx
@@ -1,4 +1,24 @@
+import Link from "next/link";
 
 export default function socials(){
-  return <p>here is the about me page</p>
+  
+  return <div className="container">
+    <h1 className="head">About me</h1>
+    <p className="description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+      voluptas quae quasi amet dolorum aliquid minus quaerat error 
+      similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+      voluptas quae quasi amet dolorum aliquid minus quaerat error 
+      similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+      voluptas quae quasi amet dolorum aliquid minus quaerat error 
+      similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
+    </p>
+    <ul className="links">
+      <li className="link"><Link href={"#"}>my social #1</Link></li>
+      <li className="link"><Link href={"#"}>my social #2</Link></li>
+      <li className="link"><Link href={"#"}>my social #3</Link></li>
+    </ul>
+    </div>
 }
