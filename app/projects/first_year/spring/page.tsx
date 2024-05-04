20c874297282efba6ad9923831c395c6587e685c
ori raisfeld <raisfeldori@gmail.com>
2024-05-04 21:20:35 +0300

    changed to newer version

--- app/projects/first_year/spring/page.tsx
+++ app/projects/first_year/spring/page.tsx
@@ -1,20 +1,24 @@
 import example from './example_img.jpg';
-import Image from 'next/image';
-
+import Image from "next/image"
 export default function Page(){
-    return <div className="container">
-    <h1 className="head">Project name</h1>
-    <p className="description">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-      voluptas quae quasi amet dolorum aliquid minus quaerat error 
-      similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-      voluptas quae quasi amet dolorum aliquid minus quaerat error 
-      similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
-    </p>
-    <div className="images">
-        <Image className="image" src={example} alt=''/>
+  return <div className="container">
+  <h1 className="head">Project name</h1>
+  <p className="description">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+    voluptas quae quasi amet dolorum aliquid minus quaerat error 
+    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+    voluptas quae quasi amet dolorum aliquid minus quaerat error 
+    similique dignissimos labore, facere provident iusto illo molestiae natus deleniti soluta?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur consectetur 
+  </p>
+  <div className="images">
+    <div className="image-container">
+      <Image className="image" src={example} alt=""/>
     </div>
+    <div className="image-container">
+      <Image className="image" src={example} alt=""/>
     </div>
+  </div>
+  </div>
 }
